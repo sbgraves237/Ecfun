f3793e6ab330bf8c08e3840b56497d7e7ba58269
sbgraves237 <spencer.graves@effectivedefense.org>
2021-06-29 12:28:25 -0500

    deletedFunctions

---
+++ R/deletedFunctions.R
@@ -0,0 +1,65 @@
+readFinancialCrisisFiles <- function(files, crisisType=7, 
+                                     ...){
+  cat('This function was deleted, because it used gdata,\n')
+  cat('which was scheduled to be deleted from CRAN\n')
+  cat('with all packages that used it, and it did not\n')
+  cat('seem to be used enought to justify fixing it.\n')
+  cat('If you need it, try Ecfun 0.2-0.')
+  return(data.frame(year=0, crisisType=0))
+}
+
+readCookPVI <- function(url.=
+    "https://en.wikipedia.org/wiki/Cook_Partisan_Voting_Index"){
+  cat('This function was deleted, because it ')
+  cat('scraped information from a web site,\n')
+  cat('and the need for the information seemed insufficient ')
+  cat('to justify the work required to maintain it.\n')
+  cat('If you need it, try Ecfun 0.2-0.')
+  stop()  
+}
+readCookPVI. <- function(url.=
+  "https://en.wikipedia.org/wiki/Cook_Partisan_Voting_Index",
+  UShouse=readUShouse(), USsenate=readUSsenate(), ...){
+  cat('This function was deleted, because it ')
+  cat('scraped information from a web site,\n')
+  cat('and the need for the information seemed insufficient ')
+  cat('to justify the work required to maintain it.\n')
+  cat('If you need it, try Ecfun 0.2-0.')
+  stop()  
+}
+readUShouse <- function(url.=
+      "https://www.house.gov/representatives/",
+      nonvoting=c('American Samoa', 'District of Columbia',
+         'Guam', 'Northern Mariana Islands', 'Puerto Rico',
+         'Virgin Islands'),
+      fixNonStandard=subNonStandardNames, ...){
+  cat('This function was deleted, because it ')
+  cat('scraped information from a web site,\n')
+  cat('and the need for the information seemed insufficient ')
+  cat('to justify the work required to maintain it.\n')
+  cat('If you need it, try Ecfun 0.2-0.')
+  stop()  
+}
+readUSsenate <- function(url.=
+  "https://www.senate.gov/general/contact_information/senators_cfm.xml",
+  stateAbbreviations=Ecdat::USstateAbbreviations,
+  fixNonStandard=subNonStandardNames, ...){
+  cat('This function was deleted, because it ')
+  cat('scraped information from a web site,\n')
+  cat('and the need for the information seemed insufficient ')
+  cat('to justify the work required to maintain it.\n')
+  cat('If you need it, try Ecfun 0.2-0.')
+  stop()  
+}
+readUSstateAbbreviations <- function(url.=
+  "https://en.wikipedia.org/wiki/List_of_U.S._state_abbreviations",
+  clean=TRUE, Names=c('Name', 'Status', 'ISO', 'ANSI.letters',
+  'ANSI.digits', 'USPS', 'USCG', 'Old.GPO', 'AP', 'Other') ){
+  cat('This function was deleted, because it ')
+  cat('scraped information from a web site,\n')
+  cat('and the need for the information seemed insufficient ')
+  cat('to justify the work required to maintain it.\n')
+  cat('If you need it, try Ecfun 0.2-0.')
+  stop()  
+}
+
