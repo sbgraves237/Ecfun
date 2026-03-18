490a1d00c9fe006668324c92dd36824dd3cc4f70
sbgraves237 <spencer.graves@effectivedefense.org>
2026-03-18 05:34:53 -0500

    new fn tableCountAverage

---
+++ R/tableCountAverage.R
@@ -0,0 +1,30 @@
+tableCountAverage <- function(x, y, data, useNA = c("no", "ifany", "always"), 
+                              na.rm=TRUE, decreasing=TRUE, ...){
+##
+## 1. table 
+##
+  xTab <- table(data[, x], useNA=useNA, ...)
+## 
+## 2. sort? 
+##
+  if(!is.na(decreasing)){
+    xTab <- sort(xTab, decreasing)
+  }
+##
+## 3. compute means for each level of x 
+##
+  yMeans <- by(data, as.factor(data[, x]), 
+               function(x)mean(x[, y], na.rm=na.rm) )
+##
+## 4. Done 
+##
+  out <- data.frame(x = names(xTab), count=as.numeric(xTab), 
+                    mean=as.numeric(yMeans[names(xTab)]) )
+  xNA <- which(is.na(out[, 'x']))
+  if(length(xNA)>0){
+    inNA <- which(is.na(data[, x]))
+    out[xNA, 'mean'] <- mean(data[inNA, y], na.rm=na.rm) 
+  }
+  names(out) <- c(x, 'count', y)
+  out
+}
